d31de4786c96adb0bdf3ea409a5bcaedbea0884b
gr33ngr33n <actfun07@gmail.com>
2019-05-17 00:27:56 +0800

    using mongoDB to store session

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
+const MONGODB_URI = 'mongodb+srv://actfun07_user1:actfun07_pwuser1@cluster0-j3s72.mongodb.net/shop_mongoose';
 
 const app = express();
+const store = new MongoDBStore({
+    uri: MONGODB_URI,
+    collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -20,7 +26,11 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-    session({ secret: 'my_secret', resave: false, saveUninitialized: false })     // more details: https://github.com/expressjs/session
+    session({ 
+        secret: 'my_secret', 
+        resave: false, 
+        saveUninitialized: false, 
+        store: store })     // more details: https://github.com/expressjs/session
 );
 
 app.use((req, res, next) => {   // this will be run when there is any incoming request. it is put on top of all routes. All incoming request will trigger this middleware
@@ -38,7 +48,7 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://actfun07_user1:actfun07_pwuser1@cluster0-j3s72.mongodb.net/shop_mongoose?retryWrites=true')
+mongoose.connect(MONGODB_URI)
 .then(result => {
     console.log(result);
 
